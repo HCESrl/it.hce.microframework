3689cef86d6fd1e4be34fa9e3ac4b5807aeabc92
Marco Boffo <mboffo@hce.it>
2016-03-23 16:50:01 +0100

    development version

--- public/index.php
+++ public/index.php
@@ -2,6 +2,6 @@
 
 use it\hce\microframework\core\MicroFramework;
 
-include_once('../core/MicroFramework.php');
+include_once('../vendor/hce/microframework-core/src/MicroFramework.php');
 
 new MicroFramework();
