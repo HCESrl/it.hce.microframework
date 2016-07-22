f70dc25f42568b4f39f312aa58b44e1b73f80710
Marco Boffo <mboffo@hce.it>
2016-07-22 11:36:37 +0200

    core update + lock

--- public/index.php
+++ public/index.php
@@ -1,9 +1,6 @@
 <?php
 
-use it\hce\microframework\core\MicroFramework;
-
-include_once('../vendor/hce/microframework-core/src/MicroFramework.php');
-
 require __DIR__ . '/../vendor/autoload.php';
 
+use it\hce\microframework\core\MicroFramework;
 new MicroFramework();
