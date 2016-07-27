8512c28a5683d69a5c0ea590b5229cb1177b126e
Marco Boffo <mboffo@hce.it>
2016-07-27 14:42:56 +0200

    rtl update

--- templates/homepage.blade.php
+++ templates/homepage.blade.php
@@ -1,15 +1,5 @@
-<!DOCTYPE html>
-<html>
-<head>
-    <title>hce microframework</title>
-    <meta content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" name="viewport">
-    <link rel="stylesheet" href="{{$GLOBAL->css}}">
-    <link href='https://fonts.googleapis.com/css?family=Droid+Sans' rel='stylesheet' type='text/css'>
-</head>
-<body class="home">
+@extends('templates.master')
 
-@include('components.logo.template')
-
-<script src="{{$GLOBAL->js}}"></script>
-</body>
-</html>
+@section('components')
+    @include('components.logo.template')
+@endsection
